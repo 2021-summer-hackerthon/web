fcb5cc6499e9b5d3dd6e1c9680e15f98b0efa800
Clzzi <clzzi1109@naver.com>
2021-07-13 14:10:44 +0900

    Add: CArd

--- src/components/CardItem/CardItem.jsx
+++ src/components/CardItem/CardItem.jsx
@@ -4,9 +4,17 @@ const style = require("./CardItem.scss");
 const cx = classNames.bind(style);
 
 const CardItem = () => {
+  // 서버에서 별점 이미지 이름 좌표
   return (
     <div className={cx('CardItem')}>
-      
+      <div className={cx('CardItem-Content')}>
+        <div className={cx('CardItem-Content-Title')}>공화춘</div>
+        <div className={cx('CardItem-Content-Footer')}>
+          <div className={cx('CardItem-Content-Footer-StarStandard')}>/5</div>
+          <div className={cx('CardItem-Content-Footer-Star')}>4.4 </div>
+          <div className={cx('CardItem-Content-Footer-StarLogo')}>★</div>
+        </div>
+      </div>
     </div>
   );
 }
