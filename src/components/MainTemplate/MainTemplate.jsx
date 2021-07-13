c70b36079e8f2a74b73a9752bdfd24ac8a044b5d
krung2 <jungbin4337@dgsw.hs.kr>
2021-07-13 21:14:08 +0900

    a

--- src/components/MainTemplate/MainTemplate.jsx
+++ src/components/MainTemplate/MainTemplate.jsx
@@ -6,14 +6,26 @@ import SideContainer from "containers/SideContainer";
 import Info from "../Info";
 import { useRecoilState } from "recoil";
 import { isClickCardState } from "recoil/mapAtom";
+import thumb from '../../asset/thumb.svg';
+import { isModalState } from "recoil/modalAtom";
 
 const style = require("./MainTemplate.scss");
 const cx = classNames.bind(style);
 
 const MainTemplate = () => {
   const [isClick, setIsClick] = useRecoilState(isClickCardState);
+  const [isModal, setIsModal] = useRecoilState(isModalState);
+
+  const modalOpen = () => {
+
+    setIsModal(isModal ? false : true);
+  }
+
   return (
     <div className={cx("AppWrapper")}>
+      <div className={cx("AppWrapper-thumb")} onClick={modalOpen} >
+        <img src={thumb} className={cx("AppWrapper-thumb-svg")} />
+      </div>
       <NavContainer />
       <div className={cx("AppWrapper-SideWrapper")}>
         <SideContainer />
