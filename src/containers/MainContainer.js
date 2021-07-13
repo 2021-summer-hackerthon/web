9183d7dc45de89290ba88423bcb5e744ab618fba
Clzzi <clzzi1109@naver.com>
2021-07-14 08:32:45 +0900

    123

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -77,7 +77,7 @@ const MainContainer = () => {
   useEffect(() => {
     let container = document.getElementById("map");
     let options = {
-      center: new kakao.maps.LatLng(35.661239715391845, 128.415),
+      center: new kakao.maps.LatLng(35.6625000, 128.414119415),
       level: 3,
     };
     let map = new kakao.maps.Map(container, options);
